3a1f730d9003307cfe6bb992bd41f292a2c529d5
Jvsantiago25 <joaovictor.santiagorm5@gmail.com>
2024-02-03 01:07:48 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,13 @@ import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale/pt-BR";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, João!</h2>
         <p className="capitalize text-sm">
@@ -18,6 +20,13 @@ export default function Home() {
 
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold ">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
     </div>
   );
